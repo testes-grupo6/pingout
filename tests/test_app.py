76f1105aecf0b38c74ac3d430363ef238d28d1d3
Lucas Siqueira <lucassiqzro@gmail.com>
2018-11-02 17:12:54 -0200

    Create test for invalid date range
    Signed-off-by: Lucas Siqueira <lucassiqzro@gmail.com>
    Co-Authored-by: João Saliba <joao_salibaa@hotmail.com>
    Co-Authored-by: Amanda Muniz <amanda.muniiz@outlook.com>

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,6 @@
+from dateutil import parser
+from pingout.__init__ import create_app
+
 def test_return_200_on_root(client):
     """ Return status code 200 on root url"""
     response = client.get('/')
@@ -5,7 +8,9 @@ def test_return_200_on_root(client):
 
 def test_create_pingout(client):
     response = client.post('create-pingout')
+    response2 = client.get('create-pingout')
     assert response.status_code == 201
+    assert response2.status_code == 405
 
 def test_bad_format_pingout(client):
     uuid = '12345678aq234123ac12423a'
@@ -22,5 +27,12 @@ def test_valid_pingout(client):
     uuid = pingout.json['uuid']
     client.post(f'/{uuid}/ping')
     pingout = client.get(f'/{uuid}')
-
     assert pingout.status_code == 200
+
+def test_pingout_invalid_date_rage(client):
+    pingout = client.post('/create-pingout')
+    uuid = pingout.json['uuid']
+    initial_date = parser.parse('2018-11-02')
+    final_date = parser.parse('2018-11-01')
+    pingout = client.get(f'/{uuid}/filter/?initial_date={initial_date}&final_date={final_date}')
+    assert pingout.status_code == 404
