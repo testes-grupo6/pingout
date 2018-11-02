44cc002c837b28b10cf6ad9e0ae21bdf29ca204a
Lucas Siqueira <lucassiqzro@gmail.com>
2018-11-02 16:45:14 -0200

    Create init tests.
    Signed-off-by: Lucas Siqueira <lucassiqzro@gmail.com>
    Co-Authored-by: Amanda Muniz <amanda.muniiz@outlook.com>

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,26 @@
-
 def test_return_200_on_root(client):
     """ Return status code 200 on root url"""
     response = client.get('/')
     assert response.status_code == 200
+
+def test_create_pingout(client):
+    response = client.post('create-pingout')
+    assert response.status_code == 201
+
+def test_bad_format_pingout(client):
+    uuid = '12345678aq234123ac12423a'
+    response = client.get(f'/{uuid}')
+    assert response.status_code == 400
+
+def test_invalid_pingout(client):
+    uuid = '9e46643f534c4807adb2d7691188242a'
+    response = client.get(f'/{uuid}')
+    assert response.status_code == 404
+
+def test_valid_pingout(client):
+    pingout = client.post('/create-pingout')
+    uuid = pingout.json['uuid']
+    client.post(f'/{uuid}/ping')
+    pingout = client.get(f'/{uuid}')
+
+    assert pingout.status_code == 200
--- tests/test_filters.py
+++ tests/test_filters.py
@@ -9,10 +9,10 @@ def test_filter_pings_range_of_dates(db_collection):
     date = datetime.datetime.today().replace(second=0,
                                              microsecond=0)
     db_collection.insert_one({'uuid': uuid.hex, 'pings': [{'count': 1, "date": date}]})
-    
+
     initial = parser.parse('-'.join([str(date.year), str(date.month), str(date.day)]))
     final = parser.parse('-'.join([str(date.year), str(date.month), str(date.day)]))
-    
+
     test = filter_pings_range_of_dates(uuid.hex, db_collection, initial, final)
     assert test == [{'count': 1, "date": date.date()}]
 
@@ -21,10 +21,10 @@ def test_filter_pings_range_of_dates_error(db_collection):
     date = datetime.datetime.today().replace(second=0,
                                              microsecond=0)
     db_collection.insert_one({'uuid': uuid.hex, 'pings': [{'count': 1, "date": date}]})
-    
+
     initial = '-'.join([str(date.year), str(date.month), str(date.day)])
     final = '-'.join([str(date.year), str(date.month), str(date.day)])
-    
+
     with pytest.raises(ValueError) as excinfo:
         filter_pings_range_of_dates(uuid.hex, db_collection, initial, final)
     assert 'Invalid date type' in str(excinfo.value)
@@ -34,7 +34,7 @@ def test_filter_invalid_date_type(db_collection):
     date = 'invalid date'
     # date = datetime.datetime.today().replace(second=0, microsecond=0)
     db_collection.insert_one({'uuid': uuid.hex, 'pings': [{'count': 1, "date": date}]})
-    
+
     with pytest.raises(ValueError) as excinfo:
        filter_pings_of_date(uuid.hex, db_collection, date)
     assert 'Invalid date type' in str(excinfo.value)
@@ -44,7 +44,7 @@ def test_filter_valid_date_type(db_collection):
     # date = datetime.datetime.today().replace(second=0, microsecond=0)
     date = datetime.datetime(2018, 10, 15, 14, 2)
     db_collection.insert_one({'uuid': uuid.hex, 'pings': [{'count': 1, "date": date}]})
-    
+
     result = filter_pings_of_date(uuid.hex, db_collection, date)
 
     assert result == [{'count': 1, 'date': datetime.datetime(2018, 10, 15, 14, 2)}]
@@ -54,7 +54,7 @@ def test_filter_invalid_type_of_range_of_dates(db_collection):
     date = 'invalid date'
     # date = datetime.datetime.today().replace(second=0, microsecond=0)
     db_collection.insert_one({'uuid': uuid.hex, 'pings': [{'count': 1, "date": date}]})
-    
+
     # initial = '-'.join([str(date.year), str(date.month), str(date.day)])
     initial = 'invalid initial date'
     # final = '-'.join([str(date.year), str(date.month), str(date.day)])
@@ -69,16 +69,11 @@ def test_filter_valid_type_of_range_of_dates(db_collection):
     date = datetime.datetime.today().replace(second=0,
                                              microsecond=0)
     db_collection.insert_one({'uuid': uuid.hex, 'pings': [{'count': 1, "date": date}]})
-    
+
     # initial = datetime.datetime.today().replace(second=0,
                                             #  microsecond=0)
     initial = datetime.datetime(2018, 11, 2, 11, 31, 1)
-    print(initial)
-
     final = datetime.datetime(2018, 11, 2, 14, 31, 1)
-
-
-    print(final)
 
     test = filter_occurrences_ping_range_date(uuid.hex, db_collection, initial, final)
     assert test == {'2018-11-02': 1}
